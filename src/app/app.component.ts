25a596b711a38deaf935f81679d8084ba5900d3b
anan <nut999anan@hotmail.com>
2024-11-11 13:15:54 +0700

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 import { HomeComponent } from './home/home.component';
+import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HelloWorldComponent, HomeComponent],
+  imports: [HelloWorldComponent, HomeComponent, HousingLocationComponent],
   template: `
     <main>
       <header class="brand-name">
@@ -14,7 +14,6 @@ import { HomeComponent } from './home/home.component';
       </header>
       <section class="content">
         <app-home></app-home>
-        <app-hello-world></app-hello-world>
       </section>
     </main>
   `,
